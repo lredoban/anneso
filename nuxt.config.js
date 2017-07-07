ec4a7d1ca985325132862ef7c9ae6068680516e7
Lovasoa <maitrejedi@lovasoa.fr>
2017-07-07 09:01:58 +0200

    Lil optimization

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Annefo Protfolio' }
+      { hid: 'description', name: 'description', content: 'Annefo Portfolio' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Viga' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
